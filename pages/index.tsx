e97f3c4bdf2aaa233c7fa770544f11310dcb78e9
Tony <extracat@users.noreply.github.com>
2022-09-10 04:46:52 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Index() {
             {data.map((p: Telegram) => (  
                 <p>
                     {p.id}: 
-                    <TelegramComponent key={p.key} telegram={p} />
+                    <TelegramComponent key={p.id} telegram={p} />
                 </p>
             ))}
         </Layout>
