a96e89c79e55a53c4198cfe6a36a4fba4dd4d470
Tony <extracat@users.noreply.github.com>
2023-10-05 00:57:01 +0200

    save

--- lib/api.ts
+++ lib/api.ts
@@ -1,12 +1,12 @@
 const env = process.env.NODE_ENV
 
 const api = {
-    telegram: "https://astro-data-api.vercel.app/api/v1/telegram",
+    telegram: "https://astro-data-api.vercel.app/api/v1/telegrams",
   }
 
 if (env == "development"){
 
-  api.telegram = "http://localhost:3000/api/v1/telegram"  
+  api.telegram = "http://localhost:3000/api/v1/telegrams"  
 
 } else if (env == "production") {
   
